aa5a893fe774bc8c3ebd9a7dcfbe8118759560e8
Nalin <s.naliniraju.gmail.com>
2019-03-19 16:18:48 +0530

    hi

---
+++ training/this/this.js
@@ -0,0 +1,28 @@
+var di;
+function arraydemo(){
+ 
+
+    
+ function item(id,name,adrs){
+    
+di=id;
+this.name=name;
+this.adrs=adrs;
+this.code=function(){
+    return name+","+di
+     +","+adrs+"<br/>";
+}
+ }
+
+var item2=new item(102,"Sandeep","san");
+ var item3=new item(104,"Raju","raj");
+ console.log(item2.code());
+ console.log(item3.code());
+
+document.write(item2.code());
+document.write(item3.code());
+
+//document.write(item2.name);
+//ocument.write(Object.values(item3));
+ 
+}
